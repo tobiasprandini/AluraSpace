e1d792829e3334752de7ffb2e62c8ef85c3771c1
Tobias Prandini <tobiasprandini@gmail.com>
2025-02-22 12:10:44 -0300

    personalizando admin

--- galeria/models.py
+++ galeria/models.py
@@ -1,10 +1,22 @@
 from django.db import models
 
+from datetime import datetime
+
 class Fotografia(models.Model):
+
+    OPCOES_DE_CATEGORIA = [
+        ('NEBULOSA', 'Nebulosa'),
+        ('ESTRELA', 'Estrela'),
+        ('GALÁXIA', 'Galáxia'),
+        ('PLANETA', 'Planeta'),
+    ]
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=150, null=False, blank=False)
+    categoria = models.CharField(max_length=100, choices=OPCOES_DE_CATEGORIA, default='')
     descricao = models.TextField(null=True, blank=True)
-    foto = models.CharField(max_length=100, null=False, blank=False)
+    foto = models.ImageField(upload_to='fotos/%Y/%m/%d/', blank=True)
+    publicada = models.BooleanField(default=False)
+    data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
 
     def __str__(self):
-        return f'Fotografia [nome={self.nome}]'
+        return self.nome
--- galeria/views.py
+++ galeria/views.py
@@ -1,17 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-
+from galeria.models import Fotografia
 
 def index(request):
+    fotografias = Fotografia.objects.order_by('-data_fotografia').filter(publicada=True)
 
-    dados = {
-    1: {'nome': 'Nebulosa de Carina',
-        'legenda': 'webbtelescope.org / NASA / James Webb'},
-    2: {'nome': 'Galáxia NGC 1079',
-        'legenda': 'nasa.org / NASA / Hubble'}
-    }
+    return render(request, 'galeria/index.html', {'cards': fotografias})
 
-    return render(request, 'galeria/index.html', {'cards': dados})
+def imagem(request, foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'galeria/imagem.html', {'fotografia': fotografia})
 
-def imagem(request):
-    return render(request, 'galeria/imagem.html')
+def buscar(request):
+    fotografias = Fotografia.objects.order_by('-data_fotografia').filter(publicada=True)
+    if 'buscar' in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if nome_a_buscar:
+            fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
+
+    return render(request, 'galeria/buscar.html', {'cards': fotografias})
